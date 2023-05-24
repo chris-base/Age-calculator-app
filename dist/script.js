9d2f5b49456bc3ca55a3e00e20422cfa59f14c9b
chris-base <baseappsdev@gmail.com>
2023-05-23 23:10:44 -0500

    started input ts/styling on input

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,42 @@
 "use strict";
-console.log('init');
+const red = "hsl(0, 100%, 67%)";
+const grey = "hsl(0, 1%, 44%)";
+let showInvalidMessage;
+const checkIfValidInputs = () => {
+    let dayInput = Number(document.getElementById("dayInput").value);
+    let monthInput = Number(document.getElementById("monthInput").value);
+    let yearInput = Number(document.getElementById("yearInput").value);
+    let invalidInputText = document.getElementById("invalidInputText");
+    showInvalidMessage = false;
+    if (dayInput < 1 || dayInput > 31) {
+        document.getElementById("dayInput").style.borderColor = red;
+        document.getElementById("dayText").style.color = red;
+        showInvalidMessage = true;
+    }
+    else {
+        document.getElementById("dayInput").style.borderColor = "#aaaaaa";
+        document.getElementById("dayText").style.color = grey;
+    }
+    if (monthInput < 1 || monthInput > 12) {
+        document.getElementById("monthInput").style.borderColor = red;
+        document.getElementById("monthText").style.color = red;
+        showInvalidMessage = true;
+    }
+    else {
+        document.getElementById("monthInput").style.borderColor = "#aaaaaa";
+        document.getElementById("monthText").style.color = grey;
+    }
+    if (yearInput < 1900 || yearInput > 2023) {
+        document.getElementById("yearInput").style.borderColor = red;
+        document.getElementById("yearText").style.color = red;
+        showInvalidMessage = true;
+    }
+    else {
+        document.getElementById("yearInput").style.borderColor = "#aaaaaa";
+        document.getElementById("yearText").style.color = grey;
+    }
+    invalidInputText.style.display = showInvalidMessage ? "flex" : "none";
+};
+document.getElementById("arrowButtonContainer").addEventListener("click", function () {
+    checkIfValidInputs();
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,51 @@
-console.log('init');
+const red = "hsl(0, 100%, 67%)";
+const grey = "hsl(0, 1%, 44%)";
+let showInvalidMessage: Boolean;
+
+const checkIfValidInputs = () => {
+
+    let dayInput = Number((document.getElementById("dayInput") as HTMLInputElement).value);
+    let monthInput = Number((document.getElementById("monthInput") as HTMLInputElement).value);
+    let yearInput = Number((document.getElementById("yearInput") as HTMLInputElement).value);
+
+    let invalidInputText = (document.getElementById("invalidInputText") as HTMLParagraphElement);
+
+    showInvalidMessage = false;
+
+    if (dayInput < 1 || dayInput > 31) {
+        (document.getElementById("dayInput") as HTMLInputElement).style.borderColor = red;
+        (document.getElementById("dayText") as HTMLParagraphElement).style.color = red;
+
+        showInvalidMessage = true;
+    } else {
+        (document.getElementById("dayInput") as HTMLInputElement).style.borderColor = "#aaaaaa";
+        (document.getElementById("dayText") as HTMLParagraphElement).style.color = grey;
+    }
+
+    if (monthInput < 1 || monthInput > 12) {
+        (document.getElementById("monthInput") as HTMLInputElement).style.borderColor = red;
+        (document.getElementById("monthText") as HTMLParagraphElement).style.color = red;
+
+        showInvalidMessage = true;
+    } else {
+        (document.getElementById("monthInput") as HTMLInputElement).style.borderColor = "#aaaaaa";
+        (document.getElementById("monthText") as HTMLParagraphElement).style.color = grey;
+    }
+
+    if (yearInput < 1900 || yearInput > 2023) {
+        (document.getElementById("yearInput") as HTMLInputElement).style.borderColor = red;
+        (document.getElementById("yearText") as HTMLParagraphElement).style.color = red;
+
+        showInvalidMessage = true;
+    } else {
+        (document.getElementById("yearInput") as HTMLInputElement).style.borderColor = "#aaaaaa";
+        (document.getElementById("yearText") as HTMLParagraphElement).style.color = grey;
+    }
+
+    invalidInputText.style.display = showInvalidMessage ? "flex" : "none";
+
+}
+
+document.getElementById("arrowButtonContainer")!.addEventListener("click", function () {
+    checkIfValidInputs();
+});
